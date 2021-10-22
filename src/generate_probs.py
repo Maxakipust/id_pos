c7272f5de0935aad56bddc63dbc80d89f72bb34c
maxkipust <maxakipust@gmail.com>
2021-10-22 17:05:17 -0400

    spelling

--- src/generate_probs.py
+++ src/generate_probs.py
@@ -36,39 +36,39 @@ for taga in tags:
     for tagb in tags:
         transitionCounts[(taga, tagb)] = 0
 
-emmisionCounts = {}
+emissionCounts = {}
 for tag in tags:
-    emmisionCounts[tag] = {'UNK': 0}
+    emissionCounts[tag] = {'UNK': 0}
 
 transitionTotals = {}
 for tag in tags:
     transitionTotals[tag] = 0
 
-emmisionTotals = {'UNK': 0}
+emissionTotals = {'UNK': 0}
 
 #init the count dicts for specific contexts
-context_transition_counts = {}
+contextTransitionCounts = {}
 for context in contexts:
-    context_transition_counts[context] = {}
+    contextTransitionCounts[context] = {}
     for taga in tags:
         for tagb in tags:
-            context_transition_counts[context][(taga, tagb)] = 0
+            contextTransitionCounts[context][(taga, tagb)] = 0
 
-context_emission_counts = {}
+contextEmissionCounts = {}
 for context in contexts:
-    context_emission_counts[context] = {}
+    contextEmissionCounts[context] = {}
     for tag in tags:
-        context_emission_counts[context][tag] = {'UNK':0}
+        contextEmissionCounts[context][tag] = {'UNK':0}
 
-context_transition_totals = {}
+contextTransitionTotals = {}
 for context in contexts:
-    context_transition_totals[context] = {}
+    contextTransitionTotals[context] = {}
     for tag in tags:
-        context_transition_totals[context][tag] = 0
+        contextTransitionTotals[context][tag] = 0
 
-context_emmision_totals = {}
+contexEmissionTotals = {}
 for context in contexts:
-    context_emmision_totals[context] = {'UNK': 0}
+    contexEmissionTotals[context] = {'UNK': 0}
 
 #remove numbers from words
 def cleanUpWord(id):
@@ -88,14 +88,14 @@ with open('data/orig_training_data.csv', newline='') as csvfile:
         if row['IDENTIFIER'] != prevId:
             prevId = row['IDENTIFIER']
             #get rid of case and add start and end words
-            identiferArr = ('SOI ' +row['IDENTIFIER'].lower() + ' EOI').split()
+            identifierArr = ('SOI ' +row['IDENTIFIER'].lower() + ' EOI').split()
             #extract the grammar from the dataset
             posArr = ('SOI ' + row['GRAMMAR_PATTERN'] + ' EOI').split()
             #extract the context from the dataset
             context = contexts[int(row['CONTEXT'])-1]
-            for i in range(0, len(identiferArr)-1):
+            for i in range(0, len(identifierArr)-1):
                 #clean up the word
-                id = cleanUpWord(identiferArr[i])
+                id = cleanUpWord(identifierArr[i])
                 
                 pos = posArr[i]
                 nextpos = posArr[i+1]
@@ -104,47 +104,47 @@ with open('data/orig_training_data.csv', newline='') as csvfile:
                 transitionCounts[(pos, nextpos)] = transitionCounts[(pos, nextpos)]+1
                 transitionTotals[pos] = transitionTotals[pos] + 1
 
-                emmisionCounts[pos][id] = emmisionCounts[pos][id] + 1 if id in emmisionCounts[pos] else 1
-                emmisionTotals[id] = emmisionTotals[id] + 1 if id in emmisionTotals else 1
+                emissionCounts[pos][id] = emissionCounts[pos][id] + 1 if id in emissionCounts[pos] else 1
+                emissionTotals[id] = emissionTotals[id] + 1 if id in emissionTotals else 1
 
                 #record context
-                context_transition_counts[context][(pos, nextpos)] = context_transition_counts[context][(pos, nextpos)]+1
-                context_transition_totals[context][pos] = context_transition_totals[context][pos] + 1
+                contextTransitionCounts[context][(pos, nextpos)] = contextTransitionCounts[context][(pos, nextpos)]+1
+                contextTransitionTotals[context][pos] = contextTransitionTotals[context][pos] + 1
 
-                context_emission_counts[context][pos][id] = context_emission_counts[context][pos][id] + 1 if id in context_emission_counts[context][pos] else 1
-                context_emmision_totals[context][id] = context_emmision_totals[context][id] + 1 if id in context_emmision_totals[context] else 1
+                contextEmissionCounts[context][pos][id] = contextEmissionCounts[context][pos][id] + 1 if id in contextEmissionCounts[context][pos] else 1
+                contexEmissionTotals[context][id] = contexEmissionTotals[context][id] + 1 if id in contexEmissionTotals[context] else 1
 
 
 
 #replace any word with a global total count less then unkThreshold with UNK token in each of the other counts and totals
 toDel = []
-emmisionTotalsUnkCount = 0
-for word in emmisionTotals:
-    if emmisionTotals[word] <= unkThreshold:
-        emmisionTotalsUnkCount = emmisionTotalsUnkCount + emmisionTotals[word]
+emissionTotalsUnkCount = 0
+for word in emissionTotals:
+    if emissionTotals[word] <= unkThreshold:
+        emissionTotalsUnkCount = emissionTotalsUnkCount + emissionTotals[word]
         toDel.append(word)
         for tag in tags:
-            if word in emmisionCounts[tag]:
-                if 'UNK' not in emmisionCounts[tag]:
-                    emmisionCounts[tag]['UNK'] = 0
-                emmisionCounts[tag]['UNK'] = emmisionCounts[tag]['UNK'] + emmisionCounts[tag][word]
-                del emmisionCounts[tag][word]
+            if word in emissionCounts[tag]:
+                if 'UNK' not in emissionCounts[tag]:
+                    emissionCounts[tag]['UNK'] = 0
+                emissionCounts[tag]['UNK'] = emissionCounts[tag]['UNK'] + emissionCounts[tag][word]
+                del emissionCounts[tag][word]
         for context in contexts:
-            if word in context_emmision_totals[context]:
-                if 'UNK' not in context_emmision_totals[context]:
-                    context_emmision_totals[context]['UNK'] = 0
-                context_emmision_totals[context]['UNK'] = context_emmision_totals[context]['UNK'] + context_emmision_totals[context][word]
-                del context_emmision_totals[context][word]
+            if word in contexEmissionTotals[context]:
+                if 'UNK' not in contexEmissionTotals[context]:
+                    contexEmissionTotals[context]['UNK'] = 0
+                contexEmissionTotals[context]['UNK'] = contexEmissionTotals[context]['UNK'] + contexEmissionTotals[context][word]
+                del contexEmissionTotals[context][word]
             for tag in tags:
-                if word in context_emission_counts[context][tag]:
-                    if 'UNK' not in context_emission_counts[context][tag]:
-                        context_emission_counts[context][tag]['UNK'] = 0
-                    context_emission_counts[context][tag]['UNK'] = context_emission_counts[context][tag]['UNK'] + context_emission_counts[context][tag][word]
-                    del context_emission_counts[context][tag][word]
+                if word in contextEmissionCounts[context][tag]:
+                    if 'UNK' not in contextEmissionCounts[context][tag]:
+                        contextEmissionCounts[context][tag]['UNK'] = 0
+                    contextEmissionCounts[context][tag]['UNK'] = contextEmissionCounts[context][tag]['UNK'] + contextEmissionCounts[context][tag][word]
+                    del contextEmissionCounts[context][tag][word]
 # print("deleting", toDel)
 for word in toDel:
-    del emmisionTotals[word]
-emmisionTotals['UNK'] = emmisionTotalsUnkCount
+    del emissionTotals[word]
+emissionTotals['UNK'] = emissionTotalsUnkCount
 
 #calculate probablilites
 transitionProbs = {}
@@ -155,40 +155,40 @@ for taga in tags:
         except ZeroDivisionError:
             transitionProbs[(taga, tagb)] = defaultProb
 
-emmisionProbs = {}
+emissionProbs = {}
 for tag in tags:
-    emmisionProbs[tag] = {}
-    for id in emmisionCounts[tag]:
+    emissionProbs[tag] = {}
+    for id in emissionCounts[tag]:
         try:
-            emmisionProbs[tag][id] =  emmisionCounts[tag][id] / emmisionTotals[id] 
+            emissionProbs[tag][id] =  emissionCounts[tag][id] / emissionTotals[id] 
         except ZeroDivisionError:
             transitionProbs[tag][id] = defaultProb
 
-context_transition_probs = {}
+contextTransitionProbs = {}
 for context in contexts:
-    context_transition_probs[context] = {}
+    contextTransitionProbs[context] = {}
     for taga in tags:
         for tagb in tags:
             try:
-                context_transition_probs[context][(taga, tagb)] =  context_transition_counts[context][(taga,tagb)]/context_transition_totals[context][taga] 
+                contextTransitionProbs[context][(taga, tagb)] =  contextTransitionCounts[context][(taga,tagb)]/contextTransitionTotals[context][taga] 
             except ZeroDivisionError:
-                context_transition_probs[context][(taga,tagb)] = defaultProb
+                contextTransitionProbs[context][(taga,tagb)] = defaultProb
 
-context_emmision_probs = {}
+contextemissionProbs = {}
 for context in contexts:
-    context_emmision_probs[context] = {}
+    contextemissionProbs[context] = {}
     for tag in tags:
-        context_emmision_probs[context][tag] = {}
-        for id in context_emission_counts[context][tag]:
+        contextemissionProbs[context][tag] = {}
+        for id in contextEmissionCounts[context][tag]:
             try:
-                context_emmision_probs[context][tag][id] = context_emission_counts[context][tag][id] / context_emmision_totals[context][id]
+                contextemissionProbs[context][tag][id] = contextEmissionCounts[context][tag][id] / contexEmissionTotals[context][id]
             except ZeroDivisionError:
-                context_emmision_probs[context][tag][id] = defaultProb
+                contextemissionProbs[context][tag][id] = defaultProb
 
 
 
 # Viterbi
-def run_viterbi(insent, context):
+def runViterbi(identifier, context):
 
     ## Fill in some value for every state. But sometimes the emission probability will be 0 (e.g., 
     ## the probability that you see "the" when the tag is NN). Account for this possibility *and* for the 
@@ -197,8 +197,8 @@ def run_viterbi(insent, context):
     defaultprob =  0.00000000001
 
     ## Create the trellis and backpointer itself as a 2D np array
-    stateprobs = np.zeros(shape=(len(insent), len(tags)))
-    backpointers = np.zeros(shape=(len(insent), len(tags)))
+    stateprobs = np.zeros(shape=(len(identifier), len(tags)))
+    backpointers = np.zeros(shape=(len(identifier), len(tags)))
 
     ## Initialize state 0, which must be the beginning of sentence (BOS) tag with probability 1
     stateprobs[0,tags.index("SOI")] = 1.0
@@ -206,7 +206,7 @@ def run_viterbi(insent, context):
     ## Fill in the rest of the trellis, starting with state 1
     ## Populate both state probabilities and backpointers
 
-    for i in range(1, len(insent)):
+    for i in range(1, len(identifier)):
         #for each pos
         for (posIdx, pos) in enumerate(tags):
             #calc argmax
@@ -216,7 +216,7 @@ def run_viterbi(insent, context):
                 #get the prev final pßrob
                 prevProb = stateprobs[i-1, prevIdx]
                 #get the transition prob
-                contexttprob = context_transition_probs[context][prevPos, pos] if (prevPos, pos) in context_transition_probs[context] else defaultprob
+                contexttprob = contextTransitionProbs[context][prevPos, pos] if (prevPos, pos) in contextTransitionProbs[context] else defaultprob
                 globaltprob = transitionProbs[prevPos, pos] if (prevPos, pos) in transitionProbs else defaultprob
                 tprob = (contexttprob + globaltprob) / 2
                 #calculate the prev prob * transition and save it with the pos
@@ -225,15 +225,15 @@ def run_viterbi(insent, context):
             #get the max pos and value from prev
             argmax = max(args, key= lambda x: x[0])
             #get emission probablity for the context
-            contextWord = insent[i] #if insent[i] in context_emmision_probs[context][pos] else 'UNK'
-            if contextWord in context_emmision_probs[context][pos] and context_emmision_probs[context][pos][contextWord] != 0:
-                contextEmitProb = context_emmision_probs[context][pos][contextWord]
+            contextWord = identifier[i] #if insent[i] in contextemissionProbs[context][pos] else 'UNK'
+            if contextWord in contextemissionProbs[context][pos] and contextemissionProbs[context][pos][contextWord] != 0:
+                contextEmitProb = contextemissionProbs[context][pos][contextWord]
             else:
                 contextEmitProb = defaultProb
             #get emission probablility globally
-            globalWord = insent[i] #if insent[i] in context_emmision_probs[context][pos] else 'UNK'
-            if globalWord in emmisionProbs[pos] and emmisionProbs[pos][globalWord] != 0:
-                globalEmitProb = emmisionProbs[pos][globalWord]
+            globalWord = identifier[i] #if insent[i] in contextemissionProbs[context][pos] else 'UNK'
+            if globalWord in emissionProbs[pos] and emissionProbs[pos][globalWord] != 0:
+                globalEmitProb = emissionProbs[pos][globalWord]
             else:
                 globalEmitProb = defaultProb
             #calculate the final emit prob as the average of the context prob and the global prob
@@ -254,10 +254,10 @@ def run_viterbi(insent, context):
 
     ## Getting started
     #start at the EOI tag
-    maxprob = stateprobs[len(insent)-1,tags.index("EOI")]
-    maxprobid = backpointers[len(insent)-1,tags.index("EOI")]
+    maxprob = stateprobs[len(identifier)-1,tags.index("EOI")]
+    maxprobid = backpointers[len(identifier)-1,tags.index("EOI")]
 
-    for i in range(len(insent)-2, 0, -1):
+    for i in range(len(identifier)-2, 0, -1):
         # add the maxprob pos to posseq
         posseq = tags[maxprobid.astype(int)]+ " " + posseq
         # calculate the next maxprobid
@@ -269,15 +269,6 @@ def run_viterbi(insent, context):
 success = 0
 fail = 0
 
-def run_ensemble(type, name, context):
-    name = "%20".join(name.split())
-    response = requests.get(f"http://localhost:5000/{type}/{name}/{context}")
-    response = response.text.split(',')
-    result = []
-    for section in response:
-        result.append(section.split('|')[1])
-    return " ".join(result)
-
 #run tests
 with open('data/orig_unseen_testing_data.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -287,7 +278,7 @@ with open('data/orig_unseen_testing_data.csv', newline='') as csvfile:
             idArr = ('SOI ' +row['IDENTIFIER'].lower() + ' EOI').split()
             idArr = list(map(cleanUpWord, idArr))
             context = contexts[int(row['CONTEXT']) - 1]
-            calcPOS = run_viterbi(idArr, context).split()
+            calcPOS = runViterbi(idArr, context).split()
             actualPOS = ('BOI '+ row['GRAMMAR_PATTERN'] + ' EOI').split()
             for (index, actual) in enumerate(actualPOS):
                 calc = calcPOS[index]
@@ -299,21 +290,8 @@ with open('data/orig_unseen_testing_data.csv', newline='') as csvfile:
                     # print("expected:", actual)
                     # print("calc:", calc)
                     # print()
-            
-            # ensemble_result = run_ensemble(row["TYPE"], row["IDENTIFIER"], contexts[int(row['CONTEXT'])-1])
-            # actualPOS = actualPOS[1:-1]
-            # for (index, actual) in enumerate(actualPOS):
-            #     calc = ensemble_result[index]
-            #     if calc == actual:
-            #         ensemble_success = ensemble_success + 1
-            #     else:
-            #         ensemble_fail = ensemble_fail + 1
                     
 
 print("success",success)
 print("fail", fail)
 print("acc",success/(success+fail))
-
-# print("nltk success",ensemble_success)
-# print("nltk fail", ensemble_fail)
-# print("nltk acc",ensemble_success/(ensemble_success+ensemble_fail))
