ff5acf2ad5ad9e65ec955b91943177f0a3e8ffb8
mkipust <maxakipust@gmail.com>
2021-12-07 21:25:15 -0500

    webserver stuff

--- src/webserver.py
+++ src/webserver.py
@@ -3,12 +3,21 @@ import evaluate_pos
 import clean_ids
 
 app = Flask(__name__)
+#to start webserver cd src, run export FLASK_APP=hello, flask run
 #small webserver to expose the tagger
+base_hmm_global_emission_probs = open("../model/baseHMM/global_emission_probs.txt", "r+")
+base_hmm_global_transition_probs = open("../model/baseHMM/global_transition_probs.txt", "r+")
+base_hmm_context_emission_probs = open("../model/baseHMM/context_emission_probs.txt", "r+")
+base_hmm_context_transition_probs = open("../model/baseHMM/context_transition_probs.txt", "r+")
+tag_fn = evaluate_pos.load_probs(base_hmm_global_emission_probs, base_hmm_global_transition_probs, base_hmm_context_emission_probs, base_hmm_context_transition_probs,
+0.5,0.5,0.5,0.5)
+
+
 @app.route("/<type>/<name>/<context>")
 def tag_id(type, name, context):
+    name = name.replace("()", "")
     nameArr = clean_ids.camel_case_split(name).split(" ")
-    posStr = evaluate_pos.runViterbi(nameArr, context).split()
-    posStr = posStr[1:-1]
+    posStr = tag_fn(nameArr, context)
     result = ",".join(list(map(lambda a: f"{a[0]}|{a[1]}", zip(nameArr, posStr))))
     print(name, result)
     return result
